a43cbe4b933510f28cba4da01515f50a50c70171
Mai-xiyu <111494660+Mai-xiyu@users.noreply.github.com>
2026-02-02 02:06:23 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import starlight from '@astrojs/starlight';
 
 export default defineConfig({
   site: 'https://mai-xiyu.github.io',
-  base: '/OneKeyMiner_Docs',
+  base: '/ProJect_Docs',
   integrations: [
     starlight({
       title: 'Mai_xiyu Projects',
